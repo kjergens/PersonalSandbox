63736f2a3007bace1ad515dcdedf2a01df629f58
Katie Jergens <kjergens@amplify.com>
2015-04-20 08:35:56 -0400

    random

---
+++ js/jobdetails.js
@@ -0,0 +1,22 @@
+var jobdetails = [
+		{fee: 'Free',
+		 title: 'Data Entry',
+		 est_time: '20 min',
+		 desc: 'Maintains database by entering new and updated customer and account information.',
+		 category: 'Business',
+		 accepted: ''}
+		 ,
+		{fee: '$10',
+		 title: 'Social Media Updates',
+		 est_time: '3 times a day for 1 month',
+		 desc: 'Updates social media outlets with company information.',
+		 category: 'Technology',
+		 accepted: ''}
+		 ,
+		{fee: 'Free',
+		 title: 'Test website',
+		 est_time: '4 hours',
+		 desc: 'Use the website and identify bugs.',
+		 category: 'Technology',
+	 	 accepted: ''},
+];
