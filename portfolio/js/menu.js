cc4f1c17d5e3d04cd9b961538afb5cc3875f6a1a
Katie Jergens <kjergens@amplify.com>
2016-02-17 18:13:31 -0500

    menu fixes

--- portfolio/js/menu.js
+++ portfolio/js/menu.js
@@ -1,10 +1,14 @@
 $(function () {
     var getMenuItem = function (itemData) {
         var item = $("<div class=\"menu_item\">")  
-            .append("<a href=\"" + itemData.link + "\"><h5>" + 
+            .append("<a href=\"" + itemData.link + "\">" +
+                "<figure><img src=\"../blog/images/" + itemData.image +
+                 "\"></figure>" +
+                " <div class=\"article_title\">" + 
                 itemData.title + 
-                "</h5><figure><img src=\"../blog/images/" + itemData.image +
-                 "\"></figure>");
+                "<div class=\"article_subtitle\">" +
+                itemData.subtitle +
+                "</div></div>");
         item = item.append("</div>");
         return item;
     };
