5c3264f0d19a3e6a5b887a5569a355a86f9bdb6d
Katie Jergens <kjergens@amplify.com>
2016-02-18 00:49:59 -0500

    quiz

--- portfolio/js/quiz.js
+++ portfolio/js/quiz.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
 
+	var score=0;
+
 	$(".option1").click(function() {
 		var f = this;
 		document.getElementById("book1").style.opacity = 0;
+		document.getElementById("result1").style.opacity = 1;
 		this.style.border = "6px solid red";
 		document.getElementById("correct").style.border = "6px solid green";
+
+		if (this.classList.contains("correct")) {
+			alert("hi");
+			score++;
+		}
 	}); 
 
 });
