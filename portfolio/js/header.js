5a8684ce9b72f539dbee0fa0326b49e1506758fd
Katie Jergens <kjergens@amplify.com>
2016-02-21 10:48:03 -0500

    put header in container

--- portfolio/js/header.js
+++ portfolio/js/header.js
@@ -1,16 +1,16 @@
-var header = "<div id=\"header\">" +
-      "<span id=\"site_title\">" +
+var header = "<div class=\"container\"><div class=\"row\">" +
+      "<div class=\"col-md-7 col-sm-12\" id=\"site_title\">" +
         "<a href=\"index.html\">" +
         "<span id=\"name\">KATIE JERGENS</span> UX DESIGNER" +
-      "</a></span>" +
-      "<ul id=\"menu\">" +
+      "</a></div>" +
+      "<div class=\"col-md-5 col-sm-12\"><ul id=\"menu\">" +
         "<li><a href=\"../index.html\">Home</a></li>" +
         "<li><a href=\"../resume.html\">Resum&#233;</a></li>" +
         "<li><a href=\"../portfolio.html\">Portfolio</a></li>" +
         "<li class=\"current\"><a href=\"index.html\">Blog</a></li>" +
         "<li><a href=\"../contact.html\">Contact</a></li>" +
         "<li><a href=\"../about.html\">About Me</a></li>" +
-      "</ul></div> ";
+      "</ul></div></div></div></div> ";
 
 $(document).ready(function(){
 	document.getElementById("header_container").innerHTML = header;
