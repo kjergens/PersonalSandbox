556ce61cbd67b67dbc0eb8be6aaaf1571b17f339
Katie Jergens <kjergens@amplify.com>
2016-02-22 19:39:20 -0500

    shuffled menu, fixed other post top nav

--- portfolio/js/menu.js
+++ portfolio/js/menu.js
@@ -1,34 +1,51 @@
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+}
+
+// build menu
+var getMenuItem = function (itemData) {
+    var item = $("<div class=\"menu_item\">")  
+        .append("<a href=\"" + itemData.link + "\">" +
+            "<figure><img src=\"../blog/images/" + itemData.image +
+             "\"></figure>" +
+            " <div class=\"article_title\">" + 
+            itemData.title + 
+            "<div class=\"article_subtitle\">" +
+            itemData.subtitle +
+            "</div></div>");
+    item = item.append("</div>");
+    return item;
+};
 
 $(function () {
 
     // header
     $("#right_menu").append("<div id=\"menu_heading\">Recent Posts</div><div id=\"sidemenu\"></div>");
 
-    // each item
-    var getMenuItem = function (itemData) {
-        var item = $("<div class=\"menu_item\">")  
-            .append("<a href=\"" + itemData.link + "\">" +
-                "<figure><img src=\"../blog/images/" + itemData.image +
-                 "\"></figure>" +
-                " <div class=\"article_title\">" + 
-                itemData.title + 
-                "<div class=\"article_subtitle\">" +
-                itemData.subtitle +
-                "</div></div>");
-        item = item.append("</div>");
-        return item;
-    };
-
     var $menu = $("#sidemenu");
 
-    // loop through data
+    shuffle(data.menu);
+
+    // loop through data and build menu
     $.each(data.menu, function () {
             $menu.append(
                 getMenuItem(this)
             );}
     );
 
-    
 });
 
 
