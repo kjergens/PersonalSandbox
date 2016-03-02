8ade32f5f226aa2cc516a608c2b54146e895b7c7
Katie Jergens <kjergens@hotmail.com>
2016-03-02 16:04:19 -0500

    layout of templetatized quiz

--- portfolio/js/quiz.js
+++ portfolio/js/quiz.js
@@ -1,50 +1,76 @@
 var displayQuiz = function() {
-	/*lay out the questions and images*/
+	var setno = 1;
 	var quizmarkup = "<div class=\"row\">";
 
 	/* lay out the images */
 	quizmarkup += "<div class=\"quizimg col-sm-6\">";
-	quizmarkup += "<img src=\"" + quiz[0].img_r + "\">";
-	quizmarkup += "<img id=\"blurry\" src=\"" + quiz[0].img_h + "\">";
+
+	// loop through all question images and display them all one one top of the other
+	$.each(quiz, function () { 
+		quizmarkup += "<img id=\"img_" + setno + "_r\" src=\"" + this.img_r + "\">";
+		quizmarkup += "<img id=\"img_" + setno + "_h\" src=\"" + this.img_h + "\">";
+		setno++;
+	});
 	quizmarkup += "</div>";
 
 	/* lay out the questions */
-	quizmarkup += "<div class=\"col-sm-6\"><ul class=\"options\">";
+	quizmarkup += "<div class=\"quizopts col-sm-6\">";
 
-	$.each(quiz[0].opts, function () {
-		quizmarkup += "<li class=\"option\">" + this.title + "</li>";});
+   // loop through all question sets and display them all one one top of the other
+   	setno = 1; // reset setno, for the questions
+	$.each(quiz, function () { 
+		quizmarkup += "<ul class=\"options\" id=\"options_" + setno + "\">";
+		$.each(this.opts, function () {
+			quizmarkup += "<li class=\"option\">" + this.title + "</li>";});
+		quizmarkup += "</ul>";
+		setno++;
+	});
 
-	quizmarkup += "</ul></div></div>";
+	quizmarkup += "<div id=\"nextbtn\"></div>";
+	quizmarkup += "</div></div>";
 
 	$("#quiz").append(quizmarkup);
 }
 
-	/*
-Call this function each time an answer is selected.
-*/
-var getTotal = function() {
-	if (answered == total) {
-			document.getElementById("total_result").innerHTML += "<h3>Total: " + score + " out of " + answered + "</h3>";
-	}
-}
-
-$(document).ready(function () {
-
-	var score=0;
-	var answered = 0;
-	var total = 7;
-	var question_last_answered = false;
-
-
-    displayQuiz();
 
 /*
-Handle option clicked
+When page has been loaded.
 */
-	$(".option").click(function() {
-		$("#blurry").css("opacity", "0");
-		alert(clicked);
-	}); 
+$(document).ready(function () {
+
+/*
+Display the quiz that is stored in quiz variable. Need to include a .js in the .html
+*/
+    displayQuiz();
+/*
+Start counting at highest number and go down. The question sets are loaded on top
+of each other so highest number on top and goes down.
+*/
+    var currentsetno = quiz.length;
+    var displayno = quiz.length-currentsetno+1; /*figure out the num to display. confusing. :/ */
+
+/*
+When you click an option, remove the blurry image to reveal the answer.
+*/
+$(".option").click(function() {
+	$("#img_" + currentsetno + "_h").css("opacity", "0");
+	$("#nextbtn").css("opacity", "1");
+	alert(this);
+}); 
+
+/*
+When click Next btn, remove last answer imge to reveal the new image.
+Decrease the currentsetno.
+*/
+$("#nextbtn").click(function() {
+	var currentoptions = $("#options_" + currentsetno);
+	var currentimg = $("#img_" + currentsetno + "_r");
+
+	currentimg.css("opacity", "0");
+	currentoptions.css("opacity", "0");
+	$("#nextbtn").css("opacity", "0");
+	currentsetno--;
+}); 
 
 });
 
--- portfolio/js/quizdata.js
+++ portfolio/js/quizdata.js
@@ -1,5 +1,14 @@
 var quiz = [  
-	{
+		{
+		img_h: "../blog/images/bookcovers/girlintheshadowsmiriamlynchs_thumb.jpg",
+		img_r: "../blog/images/bookcovers/keepsake-23-b-j-on-her-own-janice-harrell.jpg",
+		opts: [{title:"Amber Tamblin", is_correct:"y"},
+				{title:"Wuthering Heights", is_correct:"n"},
+				{title:"To Sir With Love", is_correct:"n"},
+				{title:"Great Gatsby", is_correct:"n"}],
+		correct_opt: 0
+		},
+		{
 		img_h: "../blog/images/bookcovers/crosswinds-1-does-your-nose-get-in-the-way-arlene-erlbach.jpg",
 		img_r: "../blog/images/bookcovers/kiis-me-creep.png",
 		opts: [{title:"Sherlock Holmes", is_correct:"y"},
@@ -7,5 +16,5 @@ var quiz = [
 				{title:"To Sir With Love", is_correct:"n"},
 				{title:"Great Gatsby", is_correct:"n"}],
 		correct_opt: 0
-	}
+		}
 ];
