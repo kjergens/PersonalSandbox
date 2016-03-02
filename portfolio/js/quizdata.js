05c1fb14d0b1edd1573548698b5eb8dcbbec0d97
Katie Jergens <kjergens@hotmail.com>
2016-03-02 14:20:13 -0500

    quiz template

---
+++ portfolio/js/quizdata.js
@@ -0,0 +1,11 @@
+var quiz = [  
+	{
+		img_h: "../blog/images/bookcovers/crosswinds-1-does-your-nose-get-in-the-way-arlene-erlbach.jpg",
+		img_r: "../blog/images/bookcovers/kiis-me-creep.png",
+		opts: [{title:"Sherlock Holmes", is_correct:"y"},
+				{title:"Wuthering Heights", is_correct:"n"},
+				{title:"To Sir With Love", is_correct:"n"},
+				{title:"Great Gatsby", is_correct:"n"}],
+		correct_opt: 0
+	}
+];
